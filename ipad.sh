bbf0b1c293afa61b231b9547b3c8e5fda2fc6c31
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-09-12 14:44:33 +0800

    Update ipad.sh

--- ipad.sh
+++ ipad.sh
@@ -9,6 +9,7 @@ sudo docker run -d --name redis -p 6379:6379 redis --requirepass "123456"
 
 wget https://lucky-hall-9f9a.joe1280.workers.dev/0:/ipad.zip
 unzip  ipad.zip
+ cd ipad/
 chmod +x main
 chmod +x service.sh
 

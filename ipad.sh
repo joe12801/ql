96124c1610271ecb08b374d3bfe6d5d12d0e0298
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-09-12 15:39:47 +0800

    Update ipad.sh

--- ipad.sh
+++ ipad.sh
@@ -7,7 +7,7 @@ wget https://github.com/joe12801/ql/raw/main/kiss.sh && bash kiss.sh
 docker pull redis
 docker run -d --name redis -p 6379:6379 redis --requirepass "123456"
 
-wget https://lucky-hall-9f9a.joe1280.workers.dev/0:/ipad.zip
+wget https://file.994938.xyz/0:/ipad.zip
 unzip  ipad.zip
  cd ipad/
 chmod +x main

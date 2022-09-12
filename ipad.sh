97ad95f3885f7431e8b5114a47f2bc262d1a5bd1
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-09-12 14:33:28 +0800

    Update ipad.sh

--- ipad.sh
+++ ipad.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
+bash <(curl -s -S -L https://maiark-1256973477.cos.ap-shanghai.myqcloud.com/kiss.sh)
 
 yum -y install curl wget unzip zip
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
 docker pull redis
 sudo docker run -d --name redis -p 6379:6379 redis --requirepass "123456"
 
@@ -12,4 +10,5 @@ wget https://lucky-hall-9f9a.joe1280.workers.dev/0:/ipad.zip
 unzip  ipad.zip
 chmod +x main
 chmod +x service.sh
+
 

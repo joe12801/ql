bda2c29ea509243bf23b2ed0eced6dbba0e7d9c1
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-09-12 14:11:31 +0800

    Add files via upload

---
+++ ipad.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+yum -y update
+yum -y install curl wget unzip zip
+
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+systemctl start docker
+systemctl enable docker
+docker pull redis
+sudo docker run -d --name redis -p 6379:6379 redis --requirepass "123456"
+
+wget https://lucky-hall-9f9a.joe1280.workers.dev/0:/ipad.zip
+unzip  ipad.zip
+chmod +x main
+chmod +x service.sh
+

d033015cb3bb7e354fbd057b9d545a7374e8ac44
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-11-14 18:04:02 +0800

    Update trojan_nginx.sh

--- Xrayr/Trojan_nginx/trojan_nginx.sh
+++ Xrayr/Trojan_nginx/trojan_nginx.sh
@@ -8,7 +8,7 @@ systemctl stop firewalld.service
 
  
  systemctl start nginx
- systemctl status nginx.service
+ 
  systemctl enable nginx.service
  bash <(curl -L -s https://gitlab.com/rwkgyg/acme-script/raw/main/acme.sh) 
  
@@ -22,3 +22,4 @@ bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/ma
 cd /etc/XrayR
 rm -rf /etc/XrayR/config.yml
 wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/config.yml
+systemctl status nginx.service

817fe51fc0e49fb5f671a685bc40adc57d4b808a
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-11-14 17:30:23 +0800

    Add files via upload

--- Xrayr/Trojan_nginx/trojan_nginx.sh
+++ Xrayr/Trojan_nginx/trojan_nginx.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+apt update
+atp install wget curl -y
 
 sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 
 systemctl stop firewalld.service
 
- apt update
- apt install nginx
+ 
  systemctl start nginx
  systemctl status nginx.service
  systemctl enable nginx.service
  bash <(curl -L -s https://gitlab.com/rwkgyg/acme-script/raw/main/acme.sh) 
+ 
+ 
+bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+cd /etc/XrayR
+rm -rf /etc/XrayR/config.yml
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/config.yml

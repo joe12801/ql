9ffa7b5d0613df7dc384fc7ca7a2c4f353348966
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-11-19 16:37:06 +0800

    Update ffmpeg.sh

--- ffmpeg/ffmpeg.sh
+++ ffmpeg/ffmpeg.sh
@@ -4,7 +4,7 @@ apt-get install cron
 systemctl restart cron
 chmod  0600 /var/spool/cron/crontabs/root -R
 systemctl restart cron
-echo "0 3 * * * /root/ffmpeg.sh" >> /var/spool/cron/crontabs/root
+echo "0 3 * * * /loca/ffmpeg.sh" >> /var/spool/cron/crontabs/root
 echo "Asia/Shanghai" > /etc/timezone
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 apt install ffmpeg -y
@@ -16,9 +16,10 @@ rm -rf  Spirited-En.mp4
 rm  -rf  English*
 rm -rf  Spirited.2022*
 sleep 2
- wget http://jd29.994938.xyz:5244/d/root/gd/Spirited.2022.1080p.WEBRip.x264-RARBG.mp4 
-sleep 5
- wget http://jd29.994938.xyz:5244/d/root/gd/English.srt 
+
+ wget http://jd29.994938.xyz/d/root/gd/Spirited.2022.1080p.WEBRip.x264-RARBG.mp4 
+
+ wget http://jd29.994938.xyz/d/root/gd/English.srt
 
 str=$"\n"
 nohup ffmpeg -i Spirited.2022.1080p.WEBRip.x264-RARBG.mp4 -vf "subtitles=English.srt" -threads 2 -c:v libx264 -crf 20 -c:a aac -b:a 2727k Spirited-En.mp4 >/dev/null 2>&1 &

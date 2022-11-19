5fc014f69478386cdde60b06716fe884f799df14
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-11-19 19:16:02 +0800

    Update centos_ffmpeg.sh

--- ffmpeg/centos_ffmpeg.sh
+++ ffmpeg/centos_ffmpeg.sh
@@ -20,7 +20,7 @@ sleep 2
 sleep 5
 
  wget http://jd29.994938.xyz/d/root/gd/English.srt
-
+sleep 2
 str=$"\n"
 nohup ffmpeg -i Spirited.2022.1080p.WEBRip.x264-RARBG.mp4 -vf "subtitles=English.srt"  Spirited-En.mp4 >/dev/null 2>&1 &
 sstr=$(echo -e $str)

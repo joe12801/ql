d2ecb9e8e7f242148aae551c9415fb4d80a8453d
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-11-20 04:45:36 +0800

    Update dd-debin.sh

--- dd-debin.sh
+++ dd-debin.sh
@@ -5,4 +5,4 @@ apt-get install -y xz-utils openssl gawk file
 
 apt-get install wget -y
 
-bash <(wget --no-check-certificate -qO- 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh') -d 11 -v 64 -a -firmware -p ab87036181
+bash <(wget --no-check-certificate -qO- 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh') -d 10 -v 64 -a -firmware -p ab87036181

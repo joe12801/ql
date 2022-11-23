7c0a8cde94af9b7b305c096e44a7e768120bfb1a
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-11-24 03:40:39 +0800

    Update gdupload.sh

--- Aria2/gdupload.sh
+++ Aria2/gdupload.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+docker  stop aria2-pro
+docker rm aria2-pro
+cd /root
+rm -rf aria2-pro
 apt install wget curl ca-certificates -y
 wget -N git.io/aria2.sh && chmod +x aria2.sh && bash aria2.sh
 
 cd /root/.aria2c
+rm -rf aria2.conf core script.conf
+wget https://github.com/joe12801/ql/raw/main/Aria2/aria2.conf
+wget https://github.com/joe12801/ql/raw/main/Aria2/core
+wget https://github.com/joe12801/ql/raw/main/Aria2/script.conf
+
+chmod 755 script.conf core aria2.conf
+
+systemctl restart aria2
+
+bash <(curl -s -S -L https://rclone.org/install.sh)
+
+cd /root/.config/
+
+rm -rf rclone.conf
+
+wget https://github.com/joe12801/ql/raw/main/Aria2/rclone.conf
+
+cd /root
+

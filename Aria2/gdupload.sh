0a7ca9af22e2cb723dacd40e6f8fc5707ff122e4
joe12801 <70465495+joe12801@users.noreply.github.com>
2022-11-24 15:23:58 +0800

    Update gdupload.sh

--- Aria2/gdupload.sh
+++ Aria2/gdupload.sh
@@ -17,9 +17,10 @@ chmod 755 script.conf core aria2.conf
 systemctl restart aria2
 
 bash <(curl -s -S -L https://rclone.org/install.sh)
+rclone config
 
 sleep 2
-mkdir /root/.config/rclone -R
+#mkdir /root/.config/rclone -R
 cd /root/.config/rclone
 
 wget https://github.com/joe12801/ql/raw/main/Aria2/rclone.conf

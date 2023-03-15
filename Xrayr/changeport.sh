7d737b1a0e808405224d5dcb26b43a9495a1ceff
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-15 21:41:33 +0800

    Update changeport.sh

--- Xrayr/changeport.sh
+++ Xrayr/changeport.sh
@@ -4,7 +4,7 @@
 read -p "请输入新的端口号: " port
 
 # 使用sed命令替换端口号
-sudo sed -i "s/listen\s\+[0-9]\+\s*ssl;/listen $port ssl;/" /etc/nginx/nginx.conf
+sed -i "s/listen\s\+[0-9]\+\s*ssl;/listen $port ssl;/" /etc/nginx/nginx.conf
 
 # 重启Nginx服务
-sudo systemctl restart nginx
+systemctl restart nginx

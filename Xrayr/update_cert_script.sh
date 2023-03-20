30786f9a17534fab763dca9f8195b950e18b3baa
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-21 01:40:59 +0800

    sspanel 更新证书

--- Xrayr/update_cert_script.sh
+++ Xrayr/update_cert_script.sh
@@ -1,4 +1,8 @@
 mkdir /usr/local/cert
 cd /usr/local/cert
-
-wget 
+wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/update_cert.sh
+bash update_cert.sh
+(crontab -l ; echo "30 3 * * * /usr/local/cert/update_cert.sh") | crontab -
+#显示cronjob列表
+echo "当前的cronjob列表："
+crontab -l

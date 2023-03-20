318ad7f1f21d48134d5ee72d21bb8a7e03828503
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-21 02:41:33 +0800

    Update update_cert_script.sh

--- Xrayr/update_cert_script.sh
+++ Xrayr/update_cert_script.sh
@@ -4,6 +4,7 @@ wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/update_cert.sh
 chmod +x update_cert.sh
 bash update_cert.sh
 (crontab -l ; echo "30 3 * * * /usr/local/cert/update_cert.sh") | crontab -
+crontab -l | grep -v '/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" > /dev/null' | crontab -
 #显示cronjob列表
 echo "当前的cronjob列表："
 crontab -l

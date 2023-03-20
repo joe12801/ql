8172b88b1ecf4aa84664a9e3c0960177838d3b5c
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-21 01:46:33 +0800

    Update update_cert_script.sh

--- Xrayr/update_cert_script.sh
+++ Xrayr/update_cert_script.sh
@@ -1,6 +1,7 @@
 mkdir /usr/local/cert
 cd /usr/local/cert
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/update_cert.sh
+chmod +x update_cert.sh
 bash update_cert.sh
 (crontab -l ; echo "30 3 * * * /usr/local/cert/update_cert.sh") | crontab -
 #显示cronjob列表

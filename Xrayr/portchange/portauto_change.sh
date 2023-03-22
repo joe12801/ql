a36ec5cb2fc641d25eab3c33debb8b75f4997b53
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-23 03:12:47 +0800

    Update portauto_change.sh

--- Xrayr/portchange/portauto_change.sh
+++ Xrayr/portchange/portauto_change.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+cd /etc/nginx
+wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/node.sh
+wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/ser.sh
+wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/change.sh
+wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/portchange.sh
+
+

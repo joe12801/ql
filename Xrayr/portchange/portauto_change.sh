26628daf0efca42bd427e58832ad90eede07a4bb
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-23 20:08:19 +0800

    Update portauto_change.sh

--- Xrayr/portchange/portauto_change.sh
+++ Xrayr/portchange/portauto_change.sh
@@ -8,7 +8,7 @@ wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/node.sh
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/ser.sh
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/change.sh
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/portchage.sh
-chmod +x node.sh ser.sh change.sh portchage.sh
+chmod +x node.sh ser.sh change.sh portchage.sh port.php
 (crontab -l ; echo "0,30 * * * * /etc/nginx/change.sh") | crontab -
 #显示cronjob列表
 echo "当前的cronjob列表："

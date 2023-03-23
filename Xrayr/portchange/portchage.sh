95a1f1c3c1c2ed208a2aeac9016141ad5cf2d1e7
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-23 20:07:33 +0800

    Update portchage.sh

--- Xrayr/portchange/portchage.sh
+++ Xrayr/portchange/portchage.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # Set the configuration file path
-config_file="/etc/nginx/nginx.conf"
+#config_file="/etc/nginx/nginx.conf"
 
 # Get the current port from the configuration file
-current_port=$(grep -oP '(?<=listen\s)[0-9]+' "$config_file")
+current_port=$(grep -Eo "listen\s+[0-9]+(\s|$)" /etc/nginx/nginx.conf | awk '{print $2}')
 
 # Check if the port is being used
 if lsof -Pi :"$current_port" -sTCP:LISTEN -t >/dev/null ; then
     echo "Port $current_port is open and available."
 else
     echo "Port $current_port is closed or being used by another process."
-
-    # Generate a random port between 30000 and 65535
+   # Generate a random port between 30000 and 65535
     new_port=$(shuf -i 30000-65535 -n 1)
-
     # Replace the port in the configuration file using sed
-    sed -i -E "s/(listen\s+)[0-9]+(\s+ssl;)/\1${new_port}\2/" "$config_file"
-
-    echo "The port has been changed to $new_port in the configuration file."
+	
+sed -i "s/listen\s*${current_port}\s*ssl;/listen ${new_port} ssl;/g" /etc/nginx/nginx.conf
+ echo "The port has been changed to $new_port in the configuration file."
 fi
+
+
+
+

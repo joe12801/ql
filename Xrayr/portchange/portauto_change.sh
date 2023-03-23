8005180cda1b046c55b08d92998276ab443aa912
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-23 20:19:46 +0800

    Update portauto_change.sh

--- Xrayr/portchange/portauto_change.sh
+++ Xrayr/portchange/portauto_change.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /etc/nginx
-rm -rf node.sh ser.sh change.sh portchage.sh port.php
+rm -rf node.sh ser.sh change.sh portchage.sh port.php del_con.sh
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/install_php.sh
 chmod +x install_php.sh
 bash install_php.sh
@@ -9,9 +9,12 @@ wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/node.sh
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/ser.sh
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/change.sh
 wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/portchage.sh
-chmod +x node.sh ser.sh change.sh portchage.sh port.php
+wget https://github.com/joe12801/ql/raw/main/Xrayr/portchange/del_con.sh
+
+chmod +x node.sh ser.sh change.sh portchage.sh port.php del_con.sh
+./del_con.sh
 /etc/nginx/change.sh
-#(crontab -l ; echo "0,30 * * * * /etc/nginx/change.sh") | crontab -
+#(crontab -l ; echo "0,5 * * * * /etc/nginx/change.sh") | crontab -
 #显示cronjob列表
 echo "当前的cronjob列表："
 crontab -l

e325d817fdd0ff000b853fa6c9a96732986e223c
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-03-27 01:23:08 +0800

    批量修改百度网盘文件名

---
+++ baidu/baiduchangename.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Prompt the user for input
+read -p "请输入 path: " path
+read -p "请输入 old_char: " old_char
+read -p "请输入 new_char: " new_char
+
+# 获取文件列表
+file_list=$(BaiduPCS-Go ls | awk -F ' ' '{print $NF}' | sed 1d)
+
+BaiduPCS-Go cd $path
+
+# 批量修改文件名（将 old_char 替换为 new_char）
+for file in $file_list; do
+		  new_file="${file//$old_char/$new_char}"
+		  	    BaiduPCS-Go mv "$file" "$new_file"
+		    done
+

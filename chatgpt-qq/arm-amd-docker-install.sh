d22836bf3816496ddb96c4185522298e51bfd2e9
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-06-11 12:47:41 +0800

    Update arm-amd-docker-install.sh

--- chatgpt-qq/arm-amd-docker-install.sh
+++ chatgpt-qq/arm-amd-docker-install.sh
@@ -6,5 +6,6 @@ unzip chatgpt-qq-arm-docker.zip
 cd chatgpt-qq 
 docker-compose pull && docker-compose up -d
 echo "自己动手修改配置文件，修改配置之后重启就可以"
+rm -rf  /root/arm-amd-docker-install.sh
 ## https://chatgpt-qq.lss233.com/pei-zhi-wen-jian-jiao-cheng/jie-ru-ai-ping-tai/jie-ru-xun-fei-xing-huo-da-mo-xing
 

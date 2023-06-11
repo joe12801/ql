7867ccea75349a554fccc844604190b7cc4642fa
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-06-11 12:38:46 +0800

    Update arm-docker-install.sh

--- chatgpt-qq/arm-docker-install.sh
+++ chatgpt-qq/arm-docker-install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+apt install zip unzip 
 
-cbb6421873a7
+wget https://github.com/joe12801/ql/raw/main/chatgpt-qq/chatgpt-qq-arm-docker.zip
+zip chatgpt-qq-arm-docker.zip
+cd chatgpt-qq 
+docker-compose pull && docker-compose up -d
+echo "自己动手修改配置文件"
+

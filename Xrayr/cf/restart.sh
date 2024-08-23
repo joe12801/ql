4529a45b7117cb2ccfaeb3d3fec1fd2c9d18e945
joe12801 <70465495+joe12801@users.noreply.github.com>
2024-08-23 23:37:53 +0800

    Update restart.sh

--- Xrayr/cf/restart.sh
+++ Xrayr/cf/restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /etc/nginx
 wget https://github.com/joe12801/ql/raw/main/Xrayr/cf/chatgpt.sh
+chmod 777 chatgpt.sh
 (crontab -l ; echo "0 */6 * * * /etc/nginx/chatgpt.sh") | crontab -
 echo '更新成功'

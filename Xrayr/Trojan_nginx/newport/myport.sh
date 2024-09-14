5c6f66db1ec48d3b36dcaa74d1893d109d97a696
joe12801 <70465495+joe12801@users.noreply.github.com>
2024-09-15 01:53:37 +0800

    Update myport.sh

--- Xrayr/Trojan_nginx/newport/myport.sh
+++ Xrayr/Trojan_nginx/newport/myport.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-echo 11
+cd /etc/nginx
+rm -rf node.sh* ser.sh* nginxport.sh*  new_port.sh*  check_port.sh* ckport.sh*
+wget https://raw.githubusercontent.com/joe12801/ql/main/Xrayr/portchange/install_php.sh
+chmod +x install_php.sh
+bash install_php.sh
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/newport/ser.sh
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/newport/node.sh
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/newport/nginxport.sh
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/newport/new_port.sh
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/newport/check_port.sh
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/newport/ckport.sh
+wget https://github.com/joe12801/ql/raw/main/Xrayr/Trojan_nginx/newport/update.php
+
+
+chmod +x node.sh ser.sh nginxport.sh new_port.sh check_port.sh ckport.sh update.php
+./del_con.sh
+/etc/nginx/change.sh
+(crontab -l ; echo "0,5 * * * * /etc/nginx/change.sh") | crontab -
+#显示cronjob列表
+echo "当前的cronjob列表："
+crontab -l
+

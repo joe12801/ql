54f7b361600b23a543dc4f659fb7cfa94aa9db56
joe12801 <70465495+joe12801@users.noreply.github.com>
2025-06-03 12:27:59 +0800

    Update up_crt.sh

--- Xrayr/更新证书/up_crt.sh
+++ Xrayr/更新证书/up_crt.sh
@@ -3,5 +3,5 @@ mkdir /usr/local/cert
 cd /usr/local/cert
 wget https://raw.githubusercontent.com/joe12801/ql/refs/heads/main/Xrayr/%E6%9B%B4%E6%96%B0%E8%AF%81%E4%B9%A6/update_crt.sh
 chmod +x update_crt.sh
-bash update_crt.sh
-(crontab -l ; echo "15 12 * * * /usr/local/cert/update_cert.sh") | crontab -
+#bash update_crt.sh
+(crontab -l ; echo "30 12 * * * /usr/local/cert/update_cert.sh") | crontab -

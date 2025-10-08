0a884dd55023e4a25b258695aeed4f2d5b3ae2fd
joe12801 <70465495+joe12801@users.noreply.github.com>
2025-10-08 22:36:54 +0800

    Update up_crt.sh
    每一月执行一次

--- Xrayr/更新证书/up_crt.sh
+++ Xrayr/更新证书/up_crt.sh
@@ -6,7 +6,7 @@ wget https://raw.githubusercontent.com/joe12801/ql/refs/heads/main/Xrayr/%E6%9B%
 chmod +x update_crt.sh
 #bash update_crt.sh
 
-(crontab -l ; echo "* * */25 * * /usr/local/cert/update_crt.sh") | crontab -
+(crontab -l ; echo "0 0 1 * * /usr/local/cert/update_crt.sh") | crontab -
 
 #显示cronjob列表
 echo "当前的cronjob列表："

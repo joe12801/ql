caa8dba09674ed59d69b6c873464ddbd71c316be
joe12801 <70465495+joe12801@users.noreply.github.com>
2025-10-08 23:19:17 +0800

    Update update_crt_port.sh

--- Xrayr/Trojan_nginx/update_crt_port.sh
+++ Xrayr/Trojan_nginx/update_crt_port.sh
@@ -1,1 +1,3 @@
-
+#!/bin/bash
+bash <(curl -s -S -L https://raw.githubusercontent.com/joe12801/ql/refs/heads/main/Xrayr/%E6%9B%B4%E6%96%B0%E8%AF%81%E4%B9%A6/up_crt.sh)
+bash <(curl -s -S -L https://raw.githubusercontent.com/joe12801/ql/refs/heads/main/Xrayr/Trojan_nginx/newport/myport.sh )
